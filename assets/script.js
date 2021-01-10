c0c0ba943ae000826549736bf90527ddb9acacfd
Taylor Sanders <tsanders3515@gmail.com>
2021-01-10 12:58:07 -0900

    arrays for all characters

--- assets/script.js
+++ assets/script.js
@@ -13,12 +13,24 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//create arrays for uppercase, lowercase, numbers and special characters
+caseArray = [];
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h"];
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H"];
+numberChar = ["1", "2", "3", "4", "5", "6", "7", "8"]
+specialChar = ["!", "@", "#", "$", "*", ";", "?", ':']
+
 //Creating a function for generating the password
 function generatePassword() {
   var characters = "abcdefgh";
   var password = "";
+
+
+
   for (var i = 0; i < 8; i++) {
     var randomNumber = Math.floor(Math.random() * characters.length);
     password = password + characters[randomNumber];
   }
 }
+
+
