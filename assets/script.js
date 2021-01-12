ad73e270fb554bdba6d31b1f83911902b7eaa719
Taylor Sanders <tsanders3515@gmail.com>
2021-01-11 17:28:06 -0900

    rearranging + variables, arrays, for loops

--- assets/script.js
+++ assets/script.js
@@ -23,15 +23,18 @@ var specialChar = ["!", "@", "#", "$", "*", ";", "?", ':']
 window.onload = alert("Welcome! Please click 'Generate Password' to start!");
 
 //Creating a function for generating the password
-function generatePassword() {
-  var characters = "abcdefgh";
-  var caseArray = [];
-  var password = "";
+function getPasswordOptions() {
+
 
   //Set variables
-  var charLength = prompt("How many characters would you like your password to be?");
+  var charLength = parseInt(prompt("How many characters would you like your password to be?"));
+  if (isNaN(charLength)) {
+    alert("Please provide a valid number.");
+    return;
+  }
   if (charLength < 8 || charLength > 128) {
     alert("It is reccomended to have a password between 8 and 128 characters long! Please start over.");
+    return;
   }
 
   //declare boolean, and ask user input
@@ -40,15 +43,66 @@ function generatePassword() {
   useNumber = confirm("Do you want to use numeric characters?");
   useSpecial = confirm("Do you want to use special characters?");
 
-  for (var i = 0; i < 8; i++) {
-    var randomNumber = Math.floor(Math.random() * characters.length);
-    password = password + characters[randomNumber];
+  if (useLowerCase === false && useUpperCase === false && useNumber === false && useSpecial === false) {
+    alert("Please choose a character type.");
+    return;
   }
+  var passOptions = {
+    charLength: charLength, useLowerCase: useLowerCase, useUpperCase: useUpperCase, useNumber: useNumber, useSpecial: useSpecial
+  };
+  console.log(passOptions);
+  return passOptions;
+}
+
+function randomElement(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomChar = arr[randomIndex];
+  return randomChar;
+}
+
+function generatePassword() {
+  var options = getPasswordOptions()
+  // chararcters to be chosen for the password
+  var possibleChar = [];
+  // each type of chosen character to ensure each will be used
+  var guaranteedChar = [];
+  //actual characters for the password
+  var passArray = [];
+  if (options.useLowerCase) {
+    possibleChar = possibleChar.concat(lowerCase)
+    guaranteedChar.push(randomElement(lowerCase))
+  }
+
+  if (options.useUpperCase) {
+    possibleChar = possibleChar.concat(upperCase)
+    guaranteedChar.push(randomElement(upperCase))
+  }
+
+  if (options.useNumber) {
+    possibleChar = possibleChar.concat(numberChar)
+    guaranteedChar.push(randomElement(numberChar))
+  }
+
+  if (options.useSpecial) {
+    possibleChar = possibleChar.concat(specialChar)
+    guaranteedChar.push(randomElement(specialChar))
+  }
+
+  for (var i = 0; i < options.charLength; i++) {
+    var tempChar = randomElement(possibleChar)
+    passArray.push(tempChar)
+  }
+  for (var i = 0; i < guaranteedChar.length; i++) {
+    passArray[i] = guaranteedChar[i]
+
+  }
+  console.log(passArray)
+  return passArray.join("");
 }
 
 //generateButton.onclick = generatePassword;
 
 //Bonus - copy to clipboard
-document.querySelector("textarea").select();
-document.execCommand("Copy");
-alert("Password copied to Clipboard!");
+// document.querySelector("textarea").select();
+// document.execCommand("Copy");
+// alert("Password copied to Clipboard!");
